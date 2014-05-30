27324b64d62b59fd1507aa223c5d0d8e201d076f
Thomas Fargeix <t.fargeix@gmail.com>
2014-05-30 16:58:26 +0200

    Fix a clustering tendency in the gradient

--- tman/src/main/java/tman/system/peer/tman/ComparatorByResource.java
+++ tman/src/main/java/tman/system/peer/tman/ComparatorByResource.java
@@ -50,9 +50,9 @@ public class ComparatorByResource implements Comparator<PeerDescriptor> {
     private int compareWithBase(int base, int a, int b) {
         if (a == b) {
             return 0;
-        } else if (a < base && b > base) {
+        } else if (a <= base && b > base) {
             return 1;
-        } else if (b < base && a > base) {
+        } else if (b <= base && a > base) {
             return -1;
         } else if (Math.abs(a - base) < Math.abs(b - base)) {
             return -1;
